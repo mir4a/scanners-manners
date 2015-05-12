70868c86d300280519447ac8dcabe6842733d2f8
Miroslav Martynov <marty.mircha@gmail.com>
2015-05-12 14:11:12 +0300

    Added scan route

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
+var scanController = require('./controllers/scan');
 
 /**
  * API keys and Passport configuration.
@@ -111,6 +112,12 @@ app.post('/account/profile', passportConf.isAuthenticated, userController.postUp
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+
+
+/**
+ * Scanner routes
+ */
+app.get('/scan', scanController.index);
 
 /**
  * API examples routes.
